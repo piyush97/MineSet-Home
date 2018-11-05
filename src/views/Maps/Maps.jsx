6111b5a03413962edb82546e513f231ff1be0bbd
Piyush Mehta <me@piyushmehta.com>
2018-11-05 15:32:19 +0530

    Pushed via Push.sh By Piyush
    Signed-off-by: Piyush Mehta <me@piyushmehta.com>

--- src/views/Maps/Maps.jsx
+++ src/views/Maps/Maps.jsx
@@ -1,102 +1,10 @@
 import React from "react";
 import { Row, Col, Card, CardHeader, CardBody } from "reactstrap";
 // react plugin used to create google maps
-import {
-  withScriptjs,
-  withGoogleMap,
-  GoogleMap,
-  Marker
-} from "react-google-maps";
 
 import { PanelHeader } from "components";
-
-const MapWrapper = withScriptjs(
-  withGoogleMap(props => (
-    <GoogleMap
-      defaultZoom={13}
-      defaultCenter={{ lat: 40.748817, lng: -73.985428 }}
-      defaultOptions={{
-        scrollwheel: false,
-        styles: [
-          {
-            featureType: "water",
-            elementType: "geometry",
-            stylers: [{ color: "#e9e9e9" }, { lightness: 17 }]
-          },
-          {
-            featureType: "landscape",
-            elementType: "geometry",
-            stylers: [{ color: "#f5f5f5" }, { lightness: 20 }]
-          },
-          {
-            featureType: "road.highway",
-            elementType: "geometry.fill",
-            stylers: [{ color: "#ffffff" }, { lightness: 17 }]
-          },
-          {
-            featureType: "road.highway",
-            elementType: "geometry.stroke",
-            stylers: [{ color: "#ffffff" }, { lightness: 29 }, { weight: 0.2 }]
-          },
-          {
-            featureType: "road.arterial",
-            elementType: "geometry",
-            stylers: [{ color: "#ffffff" }, { lightness: 18 }]
-          },
-          {
-            featureType: "road.local",
-            elementType: "geometry",
-            stylers: [{ color: "#ffffff" }, { lightness: 16 }]
-          },
-          {
-            featureType: "poi",
-            elementType: "geometry",
-            stylers: [{ color: "#f5f5f5" }, { lightness: 21 }]
-          },
-          {
-            featureType: "poi.park",
-            elementType: "geometry",
-            stylers: [{ color: "#dedede" }, { lightness: 21 }]
-          },
-          {
-            elementType: "labels.text.stroke",
-            stylers: [
-              { visibility: "on" },
-              { color: "#ffffff" },
-              { lightness: 16 }
-            ]
-          },
-          {
-            elementType: "labels.text.fill",
-            stylers: [
-              { saturation: 36 },
-              { color: "#333333" },
-              { lightness: 40 }
-            ]
-          },
-          { elementType: "labels.icon", stylers: [{ visibility: "off" }] },
-          {
-            featureType: "transit",
-            elementType: "geometry",
-            stylers: [{ color: "#f2f2f2" }, { lightness: 19 }]
-          },
-          {
-            featureType: "administrative",
-            elementType: "geometry.fill",
-            stylers: [{ color: "#fefefe" }, { lightness: 20 }]
-          },
-          {
-            featureType: "administrative",
-            elementType: "geometry.stroke",
-            stylers: [{ color: "#fefefe" }, { lightness: 17 }, { weight: 1.2 }]
-          }
-        ]
-      }}
-    >
-      <Marker position={{ lat: 40.748817, lng: -73.985428 }} />
-    </GoogleMap>
-  ))
-);
+import FormInputs from "../../components/FormInputs/FormInputs";
+import CustomButton from "../../components/CustomButton/CustomButton";
 
 class FullScreenMap extends React.Component {
   render() {
@@ -105,22 +13,48 @@ class FullScreenMap extends React.Component {
         <PanelHeader size="sm" />
         <div className="content">
           <Row>
-            <Col xs={12}>
+            <Col lg={12} md={12} sm={12} xs={12} xl={12}>
               <Card>
-                <CardHeader>Google Maps</CardHeader>
-                <CardBody>
-                  <div
-                    id="map"
-                    className="map"
-                    style={{ position: "relative", overflow: "hidden" }}
-                  >
-                    <MapWrapper
-                      googleMapURL="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"
-                      loadingElement={<div style={{ height: `100%` }} />}
-                      containerElement={<div style={{ height: `100%` }} />}
-                      mapElement={<div style={{ height: `100%` }} />}
-                    />
-                  </div>
+                <CardHeader> Login </CardHeader>
+                <CardBody className="all-icons">
+                  <center>
+                    <Col lg={12} md={12} sm={12} xs={12} xl={12}>
+                      <form>
+                        <FormInputs
+                          ncols={[
+                            "col-md-5 pr-1",
+                            "col-md-3 px-1",
+                            "col-md-4 pl-1"
+                          ]}
+                          proprieties={[
+                            {
+                              label: "Mine Data",
+                              inputProps: {
+                                type: "text",
+                                disabled: true,
+                                defaultValue: "Mine Data 1"
+                              }
+                            },
+                            {
+                              label: "Username",
+                              inputProps: {
+                                type: "text",
+                                defaultValue: "Piyush"
+                              }
+                            },
+                            {
+                              label: "Password",
+                              inputProps: {
+                                type: "password",
+                                placeholder: "password"
+                              }
+                            }
+                          ]}
+                        />
+                        <CustomButton color="primary">Login in</CustomButton>
+                      </form>
+                    </Col>
+                  </center>
                 </CardBody>
               </Card>
             </Col>
