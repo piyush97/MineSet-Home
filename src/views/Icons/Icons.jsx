0c6cdc3610f884a1da6fa1b7b00cf90fcb269935
Piyush Mehta <me@piyushmehta.com>
2018-11-05 15:28:36 +0530

    Pushed via Push.sh By Piyush
    Signed-off-by: Piyush Mehta <me@piyushmehta.com>

--- src/views/Icons/Icons.jsx
+++ src/views/Icons/Icons.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Card, CardHeader, CardBody, Row, Col } from "reactstrap";
 
 import { PanelHeader } from "components";
+import FormInputs from "../../components/FormInputs/FormInputs";
+import CustomButton from "../../components/CustomButton/CustomButton";
 
 // import icons from "variables/icons";
 
@@ -12,10 +14,49 @@ class Icons extends React.Component {
         <PanelHeader size="sm" />
         <div className="content">
           <Row>
-            <Col md={12}>
+            <Col lg={12} md={12} sm={12} xs={12} xl={12}>
               <Card>
                 <CardHeader> Login </CardHeader>
-                <CardBody className="all-icons"> Login Soon </CardBody>
+                <CardBody className="all-icons">
+                  <center>
+                    <Col lg={12} md={12} sm={12} xs={12} xl={12}>
+                      <form>
+                        <FormInputs
+                          ncols={[
+                            "col-md-5 pr-1",
+                            "col-md-3 px-1",
+                            "col-md-4 pl-1"
+                          ]}
+                          proprieties={[
+                            {
+                              label: "Mine Data",
+                              inputProps: {
+                                type: "text",
+                                disabled: true,
+                                defaultValue: "Master Mine Data"
+                              }
+                            },
+                            {
+                              label: "Username",
+                              inputProps: {
+                                type: "text",
+                                defaultValue: "Piyush"
+                              }
+                            },
+                            {
+                              label: "Password",
+                              inputProps: {
+                                type: "password",
+                                placeholder: "password"
+                              }
+                            }
+                          ]}
+                        />
+                        <CustomButton color="primary">Login in</CustomButton>
+                      </form>
+                    </Col>
+                  </center>
+                </CardBody>
               </Card>
             </Col>
           </Row>
